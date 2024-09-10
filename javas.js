93793907060c0df26b077ff71be47b26bf7c89f4
Kənan Rəfiyev <kenanrefiyev0@gmail.com>
2024-09-10 13:28:27 +0400

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,70 @@
+// Task-1
+// console.log("My name is Kenan");
+
+// Task-2
+// const name = "Example"
+// console.log(name);
+
+// Task-3
+// person , $add
+
+// Task-4
+// let bookPrice = 750;
+// let bookAmount = 14;
+// console.log(bookAmount * bookPrice);
+
+// Task-5
+// let kmhSpeed = '75';
+// 1 km/saat = 1000 m / 3600 s = 1/3.6 m/s
+// let msSpeed = kmhSpeed / 3.6
+// console.log(msSpeed);
+
+// Task-6
+
+
+
+// Task-7
+
+
+// Task-8
+// let firtName = 'Kenan';
+// let lastName = 'Refiyev';
+// console.log(`Sizin adınız ${firtName} ${lastName} Tanışlığımıza çox şadam!`);
+
+// Task-9
+// let firstName = 'Ad'
+// let lastName = 'Soyad'
+// console.log(`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}`);
+
+// Task-10
+// let fullName = "Murad Orucov"
+// console.log(fullName.length)
+
+
+// Task-11
+// let firstName = "Kenan"
+// let lastName = "Refiyev"
+// fullName =
+// let firstNameIndex = fullname
+
+// Task-12
+// let fullName = prompt("Adınızı və soyadınızı daxil edin");
+// let nameParts = fullName.split(" ");
+// let firstName = nameParts[0];
+// let lastName = nameParts[1];
+// console.log(lastName + " " + firstName)
+
+
+// Task-13
+// enterName = prompt("Adınızı daxil edin");
+// console.log(`Salam, ${enterName}`);
+
+// Task-15
+// let num1 = "12345";
+// let num2 = "98765";
+// let result = num1 * num2;
+// let resultString = result.toString();
+// let sixthDigit = resultString[5];
+// console.log(sixthDigit)
+
+
